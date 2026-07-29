855bb9c8d8cd84c78be61937709a232e01f8f4e7
foxtenth <foxtenth@gmail.com>
2026-07-29 06:52:53 +0100

    some update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
+
+
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route to the Landing page
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Route to the Pet Taxi
+@app.route('/pet-taxi')
+def pet_taxi():
+    return render_template('pet_taxi.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
